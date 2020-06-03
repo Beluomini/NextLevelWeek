229ad6b415f86cbfbc964d5bf1dac93931ea15a7
Beluomini <lucasbeluomini1@gmail.com>
2020-06-02 22:07:10 -0300

    first commit

---
+++ server/src/database/migrations/01_create_items.ts
@@ -0,0 +1,16 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+    //Criar a Tabela
+    return knex.schema.createTable('items', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+    });
+}
+
+
+export async function down(knex: Knex) {
+    //Deletar a Tabela
+    return knex.schema.dropTable('items');
+}
---
+++ server/src/routes.ts
@@ -0,0 +1,15 @@
+import express, { response } from 'express';
+
+import PointsController from './controllers/pointsController';
+import ItemsController from './controllers/itemsConttroler';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+export default routes;
